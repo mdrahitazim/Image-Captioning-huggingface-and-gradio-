7630258a6d0d923bc5ad91617066c1d68c5019a6
MD Rahit Azim <163989699+mdrahitazim@users.noreply.github.com>
2024-08-02 11:26:28 +0530

    CONVERT TO RGB

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 def generate_caption(img):
-    img_input = Image.fromarray(img)
+    img_input = Image.fromarray(img).convert("RGB")
     inputs = processor(img_input, return_tensors="pt")
     out = model.generate(**inputs)
     caption = processor.decode(out[0], skip_special_tokens=True)
